8a6aebcf0e3dbde607756099b6d7e9554e5f40d5
dubaua <dubaua@gmail.com>
2018-03-24 18:31:26 +0500

    add icon component

---
+++ src/components/Icon/icon-sprite.js
@@ -0,0 +1,8 @@
+import './src/shopping-bag.svg';
+import './src/donut.svg';
+import './src/drink.svg';
+import './src/fries.svg';
+import './src/logo-compact.svg';
+import './src/logo.svg';
+import './src/pizza.svg';
+import './src/salad.svg';
---
+++ src/components/Icon/index.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  svg.icon(:width="width", :height="height", focusable="false")
+    use(:xlink:href="xhref")
+</template>
+
+<script>
+import './icon-sprite';
+
+export default {
+  name: 'Icon',
+  props: {
+    glyph: {
+      required: true,
+      type: String,
+    },
+    width: {
+      type: Number,
+    },
+    height: {
+      type: Number,
+    },
+  },
+  computed: {
+    xhref() {
+      return `#${this.glyph}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../styles/_globals";
+
+.icon {
+  pointer-events: none;
+  width: 100%;
+  height: 100%;
+  display: block;
+  fill: currentColor;
+}
+</style>
