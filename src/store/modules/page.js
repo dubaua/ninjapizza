851d4b03ea374bc77433328497d254ce28b38c04
dubaua <dubaua@gmail.com>
2018-03-25 23:33:58 +0500

    remove disableScroll

--- src/store/modules/page.js
+++ src/store/modules/page.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-shadow */
-import disableScroll from 'disable-scroll';
+// import disableScroll from 'disable-scroll';
 
 const state = {
   isMenuOpen: false,
@@ -11,51 +11,30 @@ const state = {
 const mutations = {
   openMenu(state) {
     state.isMenuOpen = true;
-    disableScroll.on();
   },
   closeMenu(state) {
     state.isMenuOpen = false;
-    disableScroll.off();
   },
   toggleMenu(state) {
     state.isMenuOpen = !state.isMenuOpen;
-    if (state.isMenuOpen) {
-      disableScroll.on();
-    } else {
-      disableScroll.off();
-    }
   },
   openCart(state) {
     state.isCartOpen = true;
-    disableScroll.on();
   },
   closeCart(state) {
     state.isCartOpen = false;
-    disableScroll.off();
   },
   toggleCart(state) {
     state.isCartOpen = !state.isCartOpen;
-    if (state.isCartOpen) {
-      disableScroll.on();
-    } else {
-      disableScroll.off();
-    }
   },
   openOrder(state) {
     state.isOrderOpen = true;
-    disableScroll.on();
   },
   closeOrder(state) {
     state.isOrderOpen = false;
-    disableScroll.off();
   },
   toggleOrder(state) {
     state.isOrderOpen = !state.isOrderOpen;
-    if (state.isOrderOpen) {
-      disableScroll.on();
-    } else {
-      disableScroll.off();
-    }
   },
 };
 
