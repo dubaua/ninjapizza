340f825e0be0957177fcdf577f8368d41e06b96e
dubaua <dubaua@gmail.com>
2018-04-04 00:07:43 +0500

    clarify store

--- src/store/index.js
+++ src/store/index.js
@@ -1,38 +1,20 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import VuexPersist from 'vuex-persist';
-// import { freeze, unfreeze } from '@/utils';
 import modules from './modules';
 
 Vue.use(Vuex);
 
-// const debug = process.env.NODE_ENV !== 'production';
-
-const badMutations = ['setMenu', 'setCart', 'scheduleAction'];
-
 const vuexLocalStorage = new VuexPersist({
   key: 'vuex',
   storage: window.localStorage,
-  reducer: state => ({
-    products: state.products,
-    banners: state.banners,
-    cart: state.cart,
-  }),
-  filter: mutation => badMutations.indexOf(mutation.type) === -1,
+  modules: ['products', 'banners', 'cart'],
 });
 
 const store = new Vuex.Store({
   modules,
-  // strict: debug,
   plugins: [vuexLocalStorage.plugin],
 });
-
-// ТУДУ сделать фриз по открытию панельки и анфриз по закрытию
-
-// store.watch(
-//   state => state.page.isMenuOpen || state.page.isCartOpen,
-//   haveToFreeze => (haveToFreeze ? freeze() : unfreeze()),
-// );
 
 const keys = ['pizza', 'desserts', 'drinks', 'salad', 'snack'];
 keys.map(key => store.dispatch('fetchProductsByKey', key));
